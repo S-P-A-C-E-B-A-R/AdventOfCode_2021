f1337d3cc8a7e0a8892c5b5ac0521cf428cc3c61
S-P-A-C-E-B-A-R <72162631+S-P-A-C-E-B-A-R@users.noreply.github.com>
2021-12-02 18:08:02 -0500

    Finished Day 2 Part 2, note that Depth is commented out.

--- Advent2021_Puzzle_02_P2/main.c
+++ Advent2021_Puzzle_02_P2/main.c
@@ -1,8 +1,99 @@
+// Advent2021_Puzzle_02_P2
+// Modified On: 12/02/2021
+// Modified By: Kenneth Meyerson
+
+// Use: ProgramName.exe "InputText.txt"
+// This program tracks the commands provided to determine
+// final depth and position and calculate their product.
+
+// Note that a third value, aim, shall be tracked.
+//   - down X increases your aim by X units.
+//   - up X decreases your aim by X units.
+//   - forward X does two things:
+//      - It increases your horizontal position by X units.
+//      - It increases your depth by your aim multiplied by X.
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+struct position
 {
-    printf("Hello world!\n");
+   int Position;
+   int Depth;
+   int Aim;
+} GPS;
+
+int main( int argc, char *argv[] )  {
+
+    // Verify Program Use
+    if( argc == 2 ) {
+
+        // Attempt to Open File
+        FILE *fptr;
+        if ((fptr = fopen(argv[1],"r")) == NULL){
+            printf("Error opening file, check file and argument");
+            exit(1);
+        }
+
+        // Reset Coords:
+        GPS.Position = 0;
+        GPS.Depth    = 0;
+        GPS.Aim      = 0;
+
+        // Read contents of file.
+        #define MAX 20
+        char str[MAX];
+        int  num;
+        int  cntLines = 0;
+        while (fscanf(fptr, " %s %d\n" , str, &num) != EOF )
+        {
+            cntLines++;
+            // Report Line Count
+            printf("[%4d] ",cntLines);
+
+            // Parse CMD
+            if (!strcmp(str,"forward"))
+            {
+                printf("CMD (%c%d) ",27,num);
+                GPS.Position += num;
+                GPS.Depth    += num * GPS.Aim;
+            }
+            else if (!strcmp(str,"up"))
+            {
+                printf("CMD (%c%d) ",24,num);
+                //GPS.Depth -= num;
+                GPS.Aim   -= num;
+            }
+            else if (!strcmp(str,"down"))
+            {
+                printf("CMD (%c%d) ",25,num);
+                //GPS.Depth += num;
+                GPS.Aim   += num;
+            }
+            else
+            {
+                printf("Error in Input!");
+                exit(1);
+            }
+
+            // Report Result
+            printf("GPS[%4d%c,%4d%c,%4d]\n",
+                   GPS.Position,
+                   ((GPS.Position >= 0) ? 27 : 26),
+                   GPS.Depth,
+                   ((GPS.Depth >= 0) ? 25: 24),
+                   GPS.Aim);
+        }
+        fclose(fptr);
+    }
+    else if( argc > 2 ) {
+       printf("Too many arguments supplied. Expected \"Program.exe Source.txt\"\n");
+    }
+    else {
+       printf("One argument expected: \"Program.exe Source.txt\"\n");
+    }
+
+    printf("Part 1: Distance * Depth = %d", GPS.Position*GPS.Depth);
     return 0;
-}
+ }
